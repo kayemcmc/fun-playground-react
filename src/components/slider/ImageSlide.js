b29ed746e2a114ab4d5643ceafee8b8c89feb326
Karen Posada <k100posada@gmail.com>
2018-06-03 17:00:42 -0500

    set up of image slider component

---
+++ src/components/slider/ImageSlide.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ImageSlide = ({url}) => {
+    const styles = {
+        backgroundImage: `url(${url})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+    };
+  return (
+    <div className="image-slide" style={styles}></div>
+  )
+}
+
+export default ImageSlide;
